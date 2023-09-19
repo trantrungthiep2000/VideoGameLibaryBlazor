04240d8f4b147efb626e6cd4b583587e7d9f0fd3
Thiep Tran Trung <thiep.tran@ntq-solution.com.vn>
2023-09-19 11:38:54 +0700

    crud video game

--- VideoGameLibary/Server/Controllers/VideoGamesController.cs
+++ VideoGameLibary/Server/Controllers/VideoGamesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using VideoGameLibary.Server.Data;
+using VideoGameLibary.Shared.Models;
 
 namespace VideoGameLibary.Server.Controllers
 {
@@ -31,6 +33,93 @@ namespace VideoGameLibary.Server.Controllers
             var videoGames = await _dataContext.VideoGames.OrderBy(vg => vg.ReleaseYear).ToListAsync();
 
             return Ok(videoGames);
+        }
+
+        /// <summary>
+        /// Get video game by id
+        /// </summary>
+        /// <param name="videoGameId">Id of video game</param>
+        /// <returns>IActionResult</returns>
+        /// CreatedBy: ThiepTT(18/09/2023)
+        [HttpGet]
+        [Route("GetVideoGameById/{videoGameId}")]
+        public async Task<IActionResult> GetVideoGameById([Required] int videoGameId)
+        {
+            var videoGame = await _dataContext.VideoGames.FirstOrDefaultAsync(videoGame => videoGame.Id == videoGameId);
+
+            if (videoGame is null) 
+            {
+                return BadRequest($"No find video game with ID: {videoGameId}");
+            }
+
+            return Ok(videoGame);
+        }
+
+        /// <summary>
+        /// Create a video game
+        /// </summary>
+        /// <param name="videoGame">VideoGame</param>
+        /// <returns>IActionResult</returns>
+        /// CreatedBy: ThiepTT(18/09/2023)
+        [HttpPost]
+        [Route("CreateVideoGame")]
+        public async Task<IActionResult> CreateVideoGame([FromBody] VideoGame videoGame)
+        {
+            _dataContext.VideoGames.Add(videoGame);
+            var result = await _dataContext.SaveChangesAsync();
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Update a video game
+        /// </summary>
+        /// <param name="videoGame">VideoGame</param>
+        /// <param name="videoGameId">Id of video game</param>
+        /// <returns>IActionResult</returns>
+        /// CreatedBy: ThiepTT(18/09/2023)
+        [HttpPut]
+        [Route("UpdateVideoGame/{videoGameId}")]
+        public async Task<IActionResult> UpdateVideoGame([FromBody] VideoGame videoGame, [Required] int videoGameId)
+        {
+            var videoGameById = await _dataContext.VideoGames.FirstOrDefaultAsync(videoGame => videoGame.Id == videoGameId);
+
+            if (videoGameById is null)
+            {
+                return BadRequest($"No find video game with ID: {videoGameId}");
+            }
+
+            videoGameById.Title = videoGame.Title;
+            videoGameById.Publisher = videoGame.Publisher;
+            videoGameById.ReleaseYear = videoGame.ReleaseYear;
+
+            _dataContext.VideoGames.Update(videoGameById);
+            var result = await _dataContext.SaveChangesAsync();
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Delete a video game
+        /// </summary>
+        /// <param name="videoGameId">Id of video game</param>
+        /// <returns>IActionResult</returns>
+        /// CreatedBy: ThiepTT(18/09/2023)
+        [HttpDelete]
+        [Route("DeleteVideoGame/{videoGameId}")]
+        public async Task<IActionResult> DeleteVideoGame([Required] int videoGameId)
+        {
+            var videoGame = await _dataContext.VideoGames.FirstOrDefaultAsync(videoGame => videoGame.Id == videoGameId);
+
+            if (videoGame is null)
+            {
+                return BadRequest($"No find video game with ID: {videoGameId}");
+            }
+
+            _dataContext.VideoGames.Remove(videoGame);
+            var result = await _dataContext.SaveChangesAsync();
+
+            return Ok(result);
         }
     }
 }
